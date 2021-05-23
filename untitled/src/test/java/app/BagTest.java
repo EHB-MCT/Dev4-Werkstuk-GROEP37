35bbefbb6613e83b5ca82766e599110ced56064c
Nabil <nabil.haouari@student.ehb.be>
2021-05-23 21:28:59 +0200

    Fix: added Class test

--- untitled/src/test/java/app/BagTest.java
+++ untitled/src/test/java/app/BagTest.java
@@ -1,6 +1,7 @@
 package app;
-
+import org.junit.jupiter.api.Test;
 public class BagTest {
+    @Test
     public static void main(String[] args) {
         Bag bag = Bag.getInstance();
         System.out.println(bag.nameproduct);
